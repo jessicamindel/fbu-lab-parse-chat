a44bfd35b30ca184d14be7aa34340371e7e6c01f
Jessie Mindel <mindel.jessie@gmail.com>
2019-07-09 10:22:47 -0700

    Add project, chat layout

---
+++ SimpleChat/app/src/main/java/com/jmindel/simplechat/ChatActivity.java
@@ -0,0 +1,32 @@
+package com.jmindel.simplechat;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.Parse;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        // Can be Level.BASIC, Level.HEADERS, or Level.BODY
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.networkInterceptors().add(httpLoggingInterceptor);
+
+        // set applicationId and server based on the values in the Heroku settings.
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("simplechat-client") // should correspond to APP_ID env variable
+                .clientBuilder(builder)
+                .server("https://my-parse-app-url.herokuapp.com/parse/").build());
+    }
+}
